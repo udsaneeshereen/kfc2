13cb45a844c5f152712a5923fe6728e14f95cc7b
Udsanee PAKDEETRAKULWONG <udsanee@webmail.npru.ac.th>
2024-02-11 11:11:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql2");
+app.use(cors());
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+app.get("/food", (req, res) => {
+  connection.query("select * from food", function (err, results, fields) {
+    res.send(results);
+  });
+});
+
+app.listen(process.env.PORT || 3000);
